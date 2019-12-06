5fe67cd4444fadfca3d2459fd8c9adf9dc9dcaa3
Teitum <jbtena@gmail.com>
2019-12-06 19:44:31 +0100

    Primera ventana V

--- Graficos/EjemploEntry.py
+++ Graficos/EjemploEntry.py
@@ -6,19 +6,32 @@ raiz=Tk()
 miFrame=Frame(raiz, width=1200, height=600)
 miFrame.pack()
 
-cuadroNombre=Entry(miFrame)
-cuadroNombre.grid(row=0, column=1, padx=10, pady=10)
+minombre=StringVar()
+
+cuadroNombre=Entry(miFrame, textvariable=minombre)
+cuadroNombre.grid(row=0, column=1, sticky="w", padx=10, pady=10)
 cuadroNombre.config(fg="red", justify="left")
 
 cuadroPassword=Entry(miFrame)
-cuadroPassword.grid(row=1, column=1, padx=10, pady=10)
-cuadroPassword.config(show="-")
+cuadroPassword.grid(row=1, column=1, sticky="w", padx=10, pady=10)
+cuadroPassword.config(show="*")
 
 cuadroApellido=Entry(miFrame)
-cuadroApellido.grid(row=2, column=1, padx=10, pady=10)
+cuadroApellido.grid(row=2, column=1, sticky="w", padx=10, pady=10)
 
 cuadroDireccion=Entry(miFrame)
-cuadroDireccion.grid(row=3, column=1, padx=10, pady=10)
+cuadroDireccion.grid(row=3, column=1, sticky="w", padx=10, pady=10)
+
+TextoComentatario=Text(miFrame, width=16, height=5)
+TextoComentatario.grid(row=4, column=1, padx=10, pady=10)
+#Introducimos un scrollbar para poder subir y bajar sobre el texto introduciro en Text
+scrollVert=Scrollbar(miFrame, command=TextoComentatario.yview)#lo ajustamos al TextoComentario
+scrollVert.grid(row=4, column=2, sticky="nsew")
+TextoComentatario.config(yscrollcommand=scrollVert.set)#Permite que al escribir, el scroll se desplaze haste la línea activa
+
+
+
+
 
 
 nombreLabel=Label(miFrame, text='Nombre :')
@@ -30,7 +43,16 @@ nombrePassword.grid(row=1, column=0, sticky="e", padx=10, pady=10)
 apellidoLabel=Label(miFrame, text='Apellidos :')
 apellidoLabel.grid(row=2, column=0, sticky='e', padx=10, pady=10)
 
-direccionLabel=Label(miFrame, text='Dirección de casa :')
+direccionLabel=Label(miFrame, text='Dirección :')
 direccionLabel.grid(row=3, column=0, sticky='e', padx=10, pady=10)
+
+comentariosLabel=Label(miFrame, text='Comentarios :')
+comentariosLabel.grid(row=4, column=0, sticky='e', padx=10, pady=10)
+
+def codigoBoton():
+	minombre.set("Juan")
+botonEnvio=Button(raiz, text="Enviar", command=codigoBoton)
+botonEnvio.pack()
+
 
 raiz.mainloop()
